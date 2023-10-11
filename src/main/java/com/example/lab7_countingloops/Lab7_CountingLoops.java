721fd7bec07dfe71d719c6a31d2980b1f63fda9b
clossocn <clossocn@mail.uc.edu>
2023-10-10 20:00:43 -0400

    Initial commit

---
+++ src/main/java/com/example/lab7_countingloops/Lab7_CountingLoops.java
@@ -0,0 +1,11 @@
+package com.example.lab7_countingloops;
+
+public class Lab7_CountingLoops {
+        public static void main(String[] args) {
+            for (int i = 10; i >= 0; i -= 2) {
+                System.out.println(i);
+            }
+        }
+}
+
+
